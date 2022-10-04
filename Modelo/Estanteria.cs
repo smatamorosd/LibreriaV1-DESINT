006f0e56f905bddcd028348a4519f99790265c8c
smatamorosd <sergiomatamoros@iescastelar.com>
2022-10-04 18:22:14 +0200

    Revert "prueba pri"
    This reverts commit dd834fc108393fc24d8868025fbd4419a6c3c363.

--- Modelo/Estanteria.cs
+++ Modelo/Estanteria.cs
@@ -1,93 +1,123 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using LibreriaV3._1.Modelo;
+using System;
 
+public class Estanteria
+{   	   
+    //Propiedades
+    //Limite de registros en el array
+    const int max_Reg = 5;
+    //Array normal con objetos Libro dentro y un valor maximo de la variable max_reg
+    Libro[] libros = new Libro[max_Reg];
 
-namespace Libreria_V2_Casa.Modelo.Estanteria { 
-    public class Estanteria
+    //Codigos de estado de la estanteria
+    public const int INTRODUCIDO = 1;
+    public const int LIBRO_EXISTE = -1;
+    public const int ESTANTERIA_LLENA = 0;
+
+    //Variable que uso, para moverme entre el array, y no crearla a nivel local de m�todo
+
+    /// <summary>
+    ///  <para>
+    ///        Introducimos por parametros un libro previamente creado.
+    ///  </para>
+    ///    
+    ///  <para>
+    ///        Comprobamos que no exista o que el array no esté lleno y si se cumple todo lo introducimos
+    ///  </para>
+    /// 
+    /// </summary>
+    /// <param name="libro">Admite parámetro del tipo Libro</param>
+    /// <returns>
+    /// Devolvera valores de : 1 en caso de libro introducido <br />
+    ///       -1 en caso de que el libro ya exista<br/>
+    ///       0 en caso de que el array este lleno
+    /// </returns>
+    public int InsertarLibro(Libro libro)
     {
-        //Propiedades
-
-        //ArrayList donde se almacena cada LIBRO
-        private List<Libro> libros = new List<Libro>();
-
-        /**
-            * Inserta un libro en un arrayList
-            * @param obLibro
-            * @return True/False
-            */
-        public Boolean InsertarLibro(Libro libro)
+        int posicionLibro = PosicionLibro(libro.Titulo);
+        if (posicionLibro != -1)
         {
-            if (BuscarLibro(libro.Titulo) == null)
-            {
-                libros.Add((libro));
-                return true;
-            }//if
-            else
-                return false;
-
-        }//insertarLibro
-
-        /**
-        * Modifica un libro en el ArrayList. Primero se localiza y si se encuentra se borra y añade uno nuevo
-        * con los datos procedentes del objeto.
-        */
-        public Boolean ModificarLibro(Libro libro)
+            return LIBRO_EXISTE;
+        }
+        for (int i = 0; i < libros.Length; i++)
         {
-            Boolean modificado = false;
-            Libro libroAux = BuscarLibro(libro.Titulo);
-            if (libro != libroAux)
+            if (libros[i] == null)
             {
-                libros.Remove(libroAux);
-                libros.Add(libro);
-                modificado = true;
+                libros[i] = libro;
+                return INTRODUCIDO;
             }
-            return modificado;
-        }//modificarLibro
+        }
+        return ESTANTERIA_LLENA;
 
+    }//insertarLibro
 
-        /**
-         * Entra por parametros una String con un titulo, y te devuelve el objeto libro exacto
-         * @param nombre
-         * @return
-         */
-        public Libro BuscarLibro(String titulo)
+    /**
+    * Introducimos por parametros un libro previamente creado.
+    * Comprobamos que no exista o que el array no est� lleno y si se cumple todo lo introducimos
+    * @param libro
+    * @return Devolvera valores de : 1 en caso de libro introducido
+    * 	    						 -1 en caso de que el libro ya exista
+    */
+    public int ModificarLibro(Libro libro)
+    {
+        int posicionLibro = PosicionLibro(libro.Titulo);
+        if (posicionLibro != -1)
         {
-            foreach (var libro in libros)
-            {
-                if (libro.Titulo.Equals(titulo))
-                {
-                    return libro;
-                }
-
-            }//foreach
-            return null;
-        }//buscarLibro
+            libros[posicionLibro] = libro;
+        }
+        return posicionLibro;
+    }//modificarLibro
 
 
-
-        /**
-         * Borra un libro del arrayList a través del nombre
-         * @param nombre
-         * @return True/False
-         */
-        public Boolean BorrarLibro(String titulo)
+    /**
+    * Entra por parametros una String con un titulo, y te devuelve el objeto libro exacto
+    * @param nombre
+    * @return El libro que coincide con el título
+    */
+    public Libro BuscarLibro(String titulo)
+    {
+        int posicionLibro = PosicionLibro(titulo);
+        if (posicionLibro != -1)
         {
-            Boolean borrado = false;
-            if (libros.Count != 0)
-            {
-                foreach (var l in libros)
-                {
-                    if (l.Titulo.Equals(titulo))
-                    {
-                        libros.Remove(l);
-                        borrado = true;
-                    }
-                    break;
-                }
+            return libros[posicionLibro];
+        }
+        return null;
+    }//buscarLibro
 
+
+
+
+    public void BorrarLibro(String titulo)
+    {
+        int posicionLibro = PosicionLibro(titulo);
+        if (posicionLibro != -1)
+        {
+            for (int i = posicionLibro; i < libros.Length - 1; i++)
+            {
+                libros[i] = libros[i + 1];
             }
-            return borrado;
-        }//borrarLibro
-    }
+        }
+        libros[libros.Length - 1] = null;
+    }//borrarLibro
+
+
+
+    /// <summary>
+    ///     Introduce un nombre de un libro por parametros y lo busca
+    /// </summary>
+    /// <param name="nombre"></param>
+    /// <returns>La posición del libro, en caso de que esté, o -1 si no está</returns>
+    private int PosicionLibro(String titulo)
+    {
+        for (int i = 0; i < libros.Length; i++)
+        {
+            if (libros[i] != null && libros[i].Titulo.Equals(titulo))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }//posicionLibro
+
 
 }
